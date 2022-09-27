46d51cfa87e5ae06661b2107867ba08174344db8
Yamamoto Taiki <c0b211598e@edu.teu.ac.jp>
2022-09-27 17:05:46 +0900

    コードです

---
+++ ex01/quiz.py
@@ -0,0 +1,24 @@
+import random
+def shutudai():
+    QA={"サザエの旦那の名前は？":["マスオ","ますお"],"カツオの妹の名前は？":["ワカメ","わかめ"],"タラオはカツオから見てどんな関係？":["甥","おい","甥っ子","おいっこ"]}
+    RQA=random.choice(list(QA.items()))
+    print(RQA[0])
+    ans=RQA[1]
+    return ans
+    
+
+def kaitou(ans):
+   ANS=input("答えるんだ：")
+   if ANS in ans:
+    print("正解!!")
+   else:
+    print("出直してこい") 
+
+
+
+if __name__ == "__main__":
+    Q=shutudai()
+    kaitou(Q)
+    
+
+
