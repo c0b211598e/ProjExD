5c71e4a940a4dfb64380baceb22f0dfe05f69fae
Yamamoto Taiki <c0b211598e@edu.teu.ac.jp>
2022-10-04 14:22:54 +0900

    電卓です

---
+++ ex02/calc.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+import tkinter.messagebox as tkm
+
+def button_click(event):
+    btn = event.widget
+    num = btn["text"]
+    tkm.showinfo(num,f"{num}のボタンがクリックされました")
+
+root = tk.Tk()
+root.title("おためしか")
+root.geometry("300x500")
+
+
+r, c = 0, 1
+for i, num in enumerate(range(9, -1, -1), 1):
+    btn = tk.Button(root, text=num, font=("Times New Roman", 30))
+    btn.bind("<1>", button_click)
+    btn.grid(row=r, column=c)
+    if i%3 == 0:
+       r += 1
+       c = 0
+    c += 1
+    
+
+
+root.mainloop()
+
