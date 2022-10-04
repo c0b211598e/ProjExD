e223ab5cb9041b6b2256b5bcee4515d221ba7c51
Yamamoto Taiki <c0b211598e@edu.teu.ac.jp>
2022-10-04 14:58:19 +0900

    電卓 練習６

--- ex02/calc.py
+++ ex02/calc.py
@@ -5,7 +5,7 @@ def button_click(event):
     btn = event.widget
     num = btn["text"]
     #tkm.showinfo(num,f"{num}のボタンがクリックされました")
-    entry.insert(tk.END, num) # 練習5
+    entry.insert(tk.END, num) #練習5
 
 
 root = tk.Tk()
@@ -13,11 +13,13 @@ root = tk.Tk()
 root.geometry("300x500")
 
 entry = tk.Entry(root, width=10, font=("Times New Roman", 40), justify="right")
-entry.grid(row=0, column=0, columnspan=3) #練習4
+entry.grid(row=0, column=0, columnspan=3) 
 
 
 r, c = 1, 0
-for i, num in enumerate(range(9, -1, -1), 1):
+numbers = list(range(9, -1, -1))
+operators = ["+"]
+for i, num in enumerate(numbers+operators, 1): #練習６
     btn = tk.Button(root, text=num, font=("Times New Roman", 30), width=4, height=2)
     btn.bind("<1>", button_click)
     btn.grid(row=r, column=c)
