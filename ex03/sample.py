7cfcd1b8b6eb14d428e6687dd7d302ce16956b2f
Yamamoto Taiki <c0b211598e@edu.teu.ac.jp>
2022-10-18 14:11:26 +0900

    練習3

---
+++ ex03/sample.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+import tkinter.messagebox as tkm
+
+def count_up():
+    global tmr, jid
+    tmr = tmr+1
+    label["text"] = tmr
+    jid = root.after(1000, count_up)
+
+
+def key_down(event):
+    global jid
+    if jid != None:
+        root.after_cancel(jid) 
+        jid = None
+        return  
+    #key = event.keysym
+    #tkm.showinfo("キー押下", f"{key}キーが押されました")
+    jid = root.after(1000,count_up)
+
+if __name__=="__main__":
+    root = tk.Tk()
+    label = tk.Label(root,font=("", 80))
+    label.pack()
+
+    jid = None
+    tmr = 0
+    #root.after(1000, count_up)
+    root.bind("<KeyPress>", key_down)
+
+    root.mainloop()
+
