6d068b88ed219ee337b85dbd45162a65c5a55a89
Yamamoto Taiki <c0b211598e@edu.teu.ac.jp>
2022-10-25 15:36:49 +0900

    練習4-6

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,6 +1,17 @@
+from inspect import FullArgSpec
 from random import randint
 import pygame as pg
 import sys
+
+def check_bound(obj_rct, scr_rct):#obj_rct:こうかとん,爆弾rct
+    
+    yoko, tate = +1, +1
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right:
+        yoko = -1
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom:
+        tate = -1
+
+    return yoko, tate
 
 
 
@@ -18,12 +29,23 @@ def main():
     tori_rct.center = 900, 400
 
     #練習5
-    bomb_sfc = pg.Surface((20, 20))
-    bomb_sfc.set_colorkey((0, 0, 0)) #透過
-    pg.draw.circle(bomb_sfc, (255, 0, 0), (10, 10), 50) #円を描く
-    bomb_rct = bomb_sfc.get_rect()
-    bomb_rct.centerx = randint(0,scrn_rct.width)
-    bomb_rct.centery = randint(0, scrn_rct.height)
+    bomb_sfc1 = pg.Surface((20, 20))
+    bomb_sfc1.set_colorkey((0, 0, 0)) #透過
+    pg.draw.circle(bomb_sfc1, (255, 0, 0), (10, 10), 10) #円を描く
+    bomb_rct1 = bomb_sfc1.get_rect()
+    bomb_rct1.centerx = randint(0,scrn_rct.width)
+    bomb_rct1.centery = randint(0, scrn_rct.height)
+
+    bomb_sfc2 = pg.Surface((100, 100))
+    bomb_sfc2.set_colorkey((0, 0, 0)) #透過
+    pg.draw.circle(bomb_sfc2, (255, 0, 0), (50, 50), 50) #円を描く
+    bomb_rct2 = bomb_sfc2.get_rect()
+    bomb_rct2.centerx = randint(0,scrn_rct.width)
+    bomb_rct2.centery = randint(0, scrn_rct.height)
+
+    #練習６
+    vx1,vy1 = +1, +1
+    vx2,vy2 = -1, -1
 
     clock = pg.time.Clock() #練習1
     while True:
@@ -36,11 +58,34 @@ def main():
         key_states = pg.key.get_pressed()
         if key_states[pg.K_UP]: tori_rct.centery -= 1
         if key_states[pg.K_DOWN]: tori_rct.centery += 1
-        if key_states[pg.K_LEFT]: tori_rct.centerx -=1
-        if key_states[pg.K_RIGHT]: tori_rct.centerx +=1
+        if key_states[pg.K_LEFT]: tori_rct.centerx -= 1
+        if key_states[pg.K_RIGHT]: tori_rct.centerx += 1
+        yoko, tate = check_bound(tori_rct, scrn_rct)#壁判定
+        if yoko == -1: 
+            if key_states[pg.K_LEFT]:
+                tori_rct.centerx += 1
+            if key_states[pg.K_RIGHT]:
+                tori_rct.centerx -= 1
+
+        if tate == -1: 
+            if key_states[pg.K_UP]:
+                tori_rct.centery += 1
+            if key_states[pg.K_DOWN]:
+                tori_rct.centery -= 1
+
         scrn_sfc.blit(tori_sfc, tori_rct)#こうかとん
 
-        scrn_sfc.blit(bomb_sfc, bomb_rct)#練習5
+        yoko, tate = check_bound(bomb_rct1, scrn_rct)#壁判定
+        vx1 *= yoko
+        vy1 *= tate
+        yoko, tate = check_bound(bomb_rct2, scrn_rct)#壁判定
+        vx2 *= yoko
+        vy2 *= tate
+
+        bomb_rct1.move_ip(vx1, vy1)
+        bomb_rct2.move_ip(vx2, vy2)
+        scrn_sfc.blit(bomb_sfc1, bomb_rct1)#練習5
+        scrn_sfc.blit(bomb_sfc2, bomb_rct2)
         
         pg.display.update()
         clock.tick(1000)
